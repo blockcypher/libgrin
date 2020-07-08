bfad8d10d4bc03ff158a7e09de16fbc1579401bd
Flowmaster <ellyssin.gimhae@gmail.com>
2020-07-07 22:54:21 -0600

    Moved AccountPathMapping to types file.

--- libwallet/api_impl_types.go
+++ libwallet/api_impl_types.go
@@ -20,11 +20,6 @@ import (
 	"github.com/blockcypher/libgrin/libwallet/slateversions"
 )
 
-type AccountPathMapping struct {
-	Label string
-	Path  string
-}
-
 // SendTXArgs Send TX API Args
 // TODO: This is here to ensure the legacy V1 API remains intact
 // remove this when v1 api is removed
