bd8421b476d6d49923f4270860e78cc269f858a5
Quentin Le Sceller <q.lesceller@gmail.com>
2020-07-16 12:29:37 -0400

    Forgot to comment stuff

--- client/secure_owner_api_test.go
+++ client/secure_owner_api_test.go
@@ -16,181 +16,177 @@ package client_test
 
 import (
 	"testing"
-
-	"github.com/blockcypher/libgrin/client"
-	"github.com/blockcypher/libgrin/libwallet"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestReal(t *testing.T) {
 	// commenting this since this can't be done on CI for now
-
-	url := "http://127.0.0.1:3420/v3/owner"
-	ownerAPI := client.NewSecureOwnerAPI(url)
-	if err := ownerAPI.Init(); err != nil {
-		assert.NoError(t, err)
-	}
-	if err := ownerAPI.Open(nil, ""); err != nil {
-		assert.NoError(t, err)
-	}
-	// NodeHeight
-	{
-		nodeHeight, err := ownerAPI.NodeHeight()
-		if err != nil {
+	/*
+		url := "http://127.0.0.1:3420/v3/owner"
+		ownerAPI := client.NewSecureOwnerAPI(url)
+		if err := ownerAPI.Init(); err != nil {
 			assert.NoError(t, err)
 		}
-		assert.NotNil(t, nodeHeight)
-	}
-	// SetTorConfig
-	{
-		torConfig := libwallet.TorConfig{
-			UseTorListener: true,
-			SocksProxyAddr: "127.0.0.1:59050",
-			SendConfigDir:  "/Users/quentin/.grin/floo/tor/sender",
+		if err := ownerAPI.Open(nil, ""); err != nil {
+			assert.NoError(t, err)
 		}
-		if err := ownerAPI.SetTorConfig(torConfig); err != nil {
-			assert.Error(t, err)
-		}
-	}
-	/*
-					// GetSlatepackAddress
-					{
-						slatepackAddress, err := ownerAPI.GetSlatepackAddress(0)
-						if err != nil {
-							assert.NoError(t, err)
-						}
-						assert.NotNil(t, slatepackAddress)
-					}
-					// GetSlatepackSecretKey
-					{
-						slatepackSecretKey, err := ownerAPI.GetSlatepackSecretKey(0)
-						if err != nil {
-							assert.NoError(t, err)
-						}
-						assert.NotNil(t, slatepackSecretKey)
-					}
-					// CreateSlatepackMessage
-					{
-						id, err := uuid.Parse("0436430c-2b02-624c-2032-570501212b00")
-						if err != nil {
-							assert.Error(t, err)
-						}
-						slateV4 := slateversions.SlateV4{
-							Ver: slateversions.VersionCompatInfoV4{
-								Version:            4,
-								BlockHeaderVersion: 2,
-							},
-							ID:  id,
-							Sta: slateversions.Standard1SlateState,
-							Off: "d202964900000000d302964900000000d402964900000000d502964900000000",
-							Amt: 6000000000,
-							Fee: 7000000,
-							Sigs: []slateversions.ParticipantDataV4{
-								{
-									Xs:    "030152d2d72e2dba7c6086ad49a219d9ff0dfe0fd993dcaea22e058c210033ce93",
-									Nonce: "031b84c5567b126440995d3ed5aaba0565d71e1834604819ff9c17f5e9d5dd078f",
-								},
-							},
-						}
-
-						var senderIndex uint32 = 0
-						recipients := make([]string, 0)
-
-						slatepackMessage, err := ownerAPI.CreateSlatepackMessage(0, slateV4, &senderIndex, recipients)
-						if err != nil {
-							assert.NoError(t, err)
-						}
-						assert.NotNil(t, slatepackMessage)
-					}
-					// SlateFromSlatepackMessage
-					{
-						message := "BEGINSLATEPACK. 8GQrdcwdLKJD28F 3a9siP7ZhZgAh7w BR2EiZHza5WMWmZ Cc8zBUemrrYRjhq j3VBwA8vYnvXXKU BDmQBN2yKgmR8mX UzvXHezfznA61d7 qFZYChhz94vd8Ew NEPLz7jmcVN2C3w wrfHbeiLubYozP2 uhLouFiYRrbe3fQ 4uhWGfT3sQYXScT dAeo29EaZJpfauh j8VL5jsxST2SPHq nzXFC2w9yYVjt7D ju7GSgHEp5aHz9R xstGbHjbsb4JQod kYLuELta1ohUwDD pvjhyJmsbLcsPei k5AQhZsJ8RJGBtY bou6cU7tZeFJvor 4LB9CBfFB3pmVWD vSLd5RPS75dcnHP nbXD8mSDZ8hJS2Q A9wgvppWzuWztJ2 dLUU8f9tLJgsRBw YZAs71HiVeg7. ENDSLATEPACK."
-
-						slateV4, err := ownerAPI.SlateFromSlatepackMessage(message, []uint32{0})
-						if err != nil {
-							assert.NoError(t, err)
-						}
-						assert.NotNil(t, slateV4)
-					}
-					// DecodeSlatepackMessage
-					{
-						message := "BEGINSLATEPACK. t9EcGgrKr1GFCQB SK2jPCxME6Hgpqx bntpQm3zKFycoPY nW4UeoL4KQ7ExNK At6EQsvpz6MjUs8 6WG8KHEbMfqufJQ ZJTw2gkcdJmJjiJ f29oGgYqqXDZox4 ujPSjrtoxCN4h3e i1sZ8dYsm3dPeXL 7VQLsYNjAefciqj ZJXPm4Pqd7VDdd4 okGBGBu3YJvYzT6 arAxeCEx66us31h AJLcDweFwyWBkW5 J1DLiYAjt5ftFTo CjpfW9KjiLq2LM5 jepXWEHJPSDAYVK 4macDZUhRbJiG6E hrQcPrJBVC716mb Hw5E1PFrE6on5wq oEmrS4j9vaB5nw8 Z9ZyXvPc2LN7tER yt6pSHZeY9EpYdY zv4bthzfRfF8ePT TMeMpV2gpgyRXQa CPD2TR. ENDSLATEPACK."
-
-						slatepack, err := ownerAPI.DecodeSlatepackMessage(message, []uint32{0})
-						if err != nil {
-							assert.NoError(t, err)
-						}
-						assert.NotNil(t, slatepack)
-					}
-
-				// Direct send to slatepack, also works with http
-				{
-					// Send the payment proof only for Tor Address
-					dest := "tgrin1980x7yg4yt6pefug45mz2suzszn8hcw2yj8z9udrnwswxk4wtjtqhdu3fs"
-					paymentProofRecipientAddress, err := slatepack.NewSlatepackAddressFromString(dest)
-					if err != nil {
-						assert.NoError(t, err)
-					}
-
-					sendArgs := libwallet.InitTxSendArgs{
-						Dest:    dest,
-						PostTx:  true,
-						Fluff:   true,
-						SkipTor: false,
-					}
-					args := libwallet.InitTxArgs{
-						SrcAcctName:                  nil,
-						Amount:                       1000000000,
-						MinimumConfirmations:         2,
-						MaxOutputs:                   500,
-						NumChangeOutputs:             1,
-						SelectionStrategyIsUseAll:    false,
-						TargetSlateVersion:           nil,
-						TTLBlocks:                    nil,
-						PaymentProofRecipientAddress: &paymentProofRecipientAddress,
-						EstimateOnly:                 nil,
-						SendArgs:                     &sendArgs,
-					}
-					slateV4, err := ownerAPI.InitSendTx(args)
-					assert.NoError(t, err)
-					assert.Equal(t, slateV4.Sta, slateversions.Standard3SlateState)
-				}
-
-			// Async send to file
-			{
-				args := libwallet.InitTxArgs{
-					SrcAcctName:                  nil,
-					Amount:                       1000000000,
-					MinimumConfirmations:         2,
-					MaxOutputs:                   500,
-					NumChangeOutputs:             1,
-					SelectionStrategyIsUseAll:    false,
-					TargetSlateVersion:           nil,
-					TTLBlocks:                    nil,
-					PaymentProofRecipientAddress: nil,
-					EstimateOnly:                 nil,
-					SendArgs:                     nil,
-				}
-				slateV4, err := ownerAPI.InitSendTx(args)
+		// NodeHeight
+		{
+			nodeHeight, err := ownerAPI.NodeHeight()
+			if err != nil {
 				assert.NoError(t, err)
-				assert.Equal(t, slateV4.Sta, slateversions.Standard1SlateState)
-				spew.Dump(slateV4)
-				assert.True(t, false)
-				// create slatepack message
-				var senderIndex uint32 = 0
-				recipients := make([]string, 0)
-
-				slatepackMessage, err := ownerAPI.CreateSlatepackMessage(0, *slateV4, &senderIndex, recipients)
-				if err != nil {
-					assert.NoError(t, err)
-				}
-				spew.Dump(slatepackMessage)
-				assert.NotNil(t, slatepackMessage)
-				// lock outputs
-				if err := ownerAPI.TxLockOutputs(*slateV4); err != nil {
-					assert.NoError(t, err)
-				}
 			}
+			assert.NotNil(t, nodeHeight)
+		}
+		// SetTorConfig
+		{
+			torConfig := libwallet.TorConfig{
+				UseTorListener: true,
+				SocksProxyAddr: "127.0.0.1:59050",
+				SendConfigDir:  "/Users/quentin/.grin/floo/tor/sender",
+			}
+			if err := ownerAPI.SetTorConfig(torConfig); err != nil {
+				assert.Error(t, err)
+			}
+		}
+
+		// GetSlatepackAddress
+		{
+			slatepackAddress, err := ownerAPI.GetSlatepackAddress(0)
+			if err != nil {
+				assert.NoError(t, err)
+			}
+			assert.NotNil(t, slatepackAddress)
+		}
+		// GetSlatepackSecretKey
+		{
+			slatepackSecretKey, err := ownerAPI.GetSlatepackSecretKey(0)
+			if err != nil {
+				assert.NoError(t, err)
+			}
+			assert.NotNil(t, slatepackSecretKey)
+		}
+		// CreateSlatepackMessage
+		{
+			id, err := uuid.Parse("0436430c-2b02-624c-2032-570501212b00")
+			if err != nil {
+				assert.Error(t, err)
+			}
+			slateV4 := slateversions.SlateV4{
+				Ver: slateversions.VersionCompatInfoV4{
+					Version:            4,
+					BlockHeaderVersion: 2,
+				},
+				ID:  id,
+				Sta: slateversions.Standard1SlateState,
+				Off: "d202964900000000d302964900000000d402964900000000d502964900000000",
+				Amt: 6000000000,
+				Fee: 7000000,
+				Sigs: []slateversions.ParticipantDataV4{
+					{
+						Xs:    "030152d2d72e2dba7c6086ad49a219d9ff0dfe0fd993dcaea22e058c210033ce93",
+						Nonce: "031b84c5567b126440995d3ed5aaba0565d71e1834604819ff9c17f5e9d5dd078f",
+					},
+				},
+			}
+
+			var senderIndex uint32 = 0
+			recipients := make([]string, 0)
+
+			slatepackMessage, err := ownerAPI.CreateSlatepackMessage(0, slateV4, &senderIndex, recipients)
+			if err != nil {
+				assert.NoError(t, err)
+			}
+			assert.NotNil(t, slatepackMessage)
+		}
+		// SlateFromSlatepackMessage
+		{
+			message := "BEGINSLATEPACK. 8GQrdcwdLKJD28F 3a9siP7ZhZgAh7w BR2EiZHza5WMWmZ Cc8zBUemrrYRjhq j3VBwA8vYnvXXKU BDmQBN2yKgmR8mX UzvXHezfznA61d7 qFZYChhz94vd8Ew NEPLz7jmcVN2C3w wrfHbeiLubYozP2 uhLouFiYRrbe3fQ 4uhWGfT3sQYXScT dAeo29EaZJpfauh j8VL5jsxST2SPHq nzXFC2w9yYVjt7D ju7GSgHEp5aHz9R xstGbHjbsb4JQod kYLuELta1ohUwDD pvjhyJmsbLcsPei k5AQhZsJ8RJGBtY bou6cU7tZeFJvor 4LB9CBfFB3pmVWD vSLd5RPS75dcnHP nbXD8mSDZ8hJS2Q A9wgvppWzuWztJ2 dLUU8f9tLJgsRBw YZAs71HiVeg7. ENDSLATEPACK."
+
+			slateV4, err := ownerAPI.SlateFromSlatepackMessage(message, []uint32{0})
+			if err != nil {
+				assert.NoError(t, err)
+			}
+			assert.NotNil(t, slateV4)
+		}
+		// DecodeSlatepackMessage
+		{
+			message := "BEGINSLATEPACK. t9EcGgrKr1GFCQB SK2jPCxME6Hgpqx bntpQm3zKFycoPY nW4UeoL4KQ7ExNK At6EQsvpz6MjUs8 6WG8KHEbMfqufJQ ZJTw2gkcdJmJjiJ f29oGgYqqXDZox4 ujPSjrtoxCN4h3e i1sZ8dYsm3dPeXL 7VQLsYNjAefciqj ZJXPm4Pqd7VDdd4 okGBGBu3YJvYzT6 arAxeCEx66us31h AJLcDweFwyWBkW5 J1DLiYAjt5ftFTo CjpfW9KjiLq2LM5 jepXWEHJPSDAYVK 4macDZUhRbJiG6E hrQcPrJBVC716mb Hw5E1PFrE6on5wq oEmrS4j9vaB5nw8 Z9ZyXvPc2LN7tER yt6pSHZeY9EpYdY zv4bthzfRfF8ePT TMeMpV2gpgyRXQa CPD2TR. ENDSLATEPACK."
+
+			slatepack, err := ownerAPI.DecodeSlatepackMessage(message, []uint32{0})
+			if err != nil {
+				assert.NoError(t, err)
+			}
+			assert.NotNil(t, slatepack)
+		}
+
+		// Direct send to slatepack, also works with http
+		{
+			// Send the payment proof only for Tor Address
+			dest := "tgrin1980x7yg4yt6pefug45mz2suzszn8hcw2yj8z9udrnwswxk4wtjtqhdu3fs"
+			paymentProofRecipientAddress, err := slatepack.NewSlatepackAddressFromString(dest)
+			if err != nil {
+				assert.NoError(t, err)
+			}
+
+			sendArgs := libwallet.InitTxSendArgs{
+				Dest:    dest,
+				PostTx:  true,
+				Fluff:   true,
+				SkipTor: false,
+			}
+			args := libwallet.InitTxArgs{
+				SrcAcctName:                  nil,
+				Amount:                       1000000000,
+				MinimumConfirmations:         2,
+				MaxOutputs:                   500,
+				NumChangeOutputs:             1,
+				SelectionStrategyIsUseAll:    false,
+				TargetSlateVersion:           nil,
+				TTLBlocks:                    nil,
+				PaymentProofRecipientAddress: &paymentProofRecipientAddress,
+				EstimateOnly:                 nil,
+				SendArgs:                     &sendArgs,
+			}
+			slateV4, err := ownerAPI.InitSendTx(args)
+			assert.NoError(t, err)
+			assert.Equal(t, slateV4.Sta, slateversions.Standard3SlateState)
+		}
+
+		// Async send to file
+		{
+			args := libwallet.InitTxArgs{
+				SrcAcctName:                  nil,
+				Amount:                       1000000000,
+				MinimumConfirmations:         2,
+				MaxOutputs:                   500,
+				NumChangeOutputs:             1,
+				SelectionStrategyIsUseAll:    false,
+				TargetSlateVersion:           nil,
+				TTLBlocks:                    nil,
+				PaymentProofRecipientAddress: nil,
+				EstimateOnly:                 nil,
+				SendArgs:                     nil,
+			}
+			slateV4, err := ownerAPI.InitSendTx(args)
+			assert.NoError(t, err)
+			assert.Equal(t, slateV4.Sta, slateversions.Standard1SlateState)
+			spew.Dump(slateV4)
+			assert.True(t, false)
+			// create slatepack message
+			var senderIndex uint32 = 0
+			recipients := make([]string, 0)
+
+			slatepackMessage, err := ownerAPI.CreateSlatepackMessage(0, *slateV4, &senderIndex, recipients)
+			if err != nil {
+				assert.NoError(t, err)
+			}
+			spew.Dump(slatepackMessage)
+			assert.NotNil(t, slatepackMessage)
+			// lock outputs
+			if err := ownerAPI.TxLockOutputs(*slateV4); err != nil {
+				assert.NoError(t, err)
+			}
+		}
 
 		// finalize tx given a slatepack
 		{
@@ -209,9 +205,9 @@ func TestReal(t *testing.T) {
 			ownerAPI.PostTx(*slateV4, true)
 
 			assert.True(t, false)
-		}*/
+		}
 
-	if err := ownerAPI.Close(nil); err != nil {
-		assert.Error(t, err)
-	}
+		if err := ownerAPI.Close(nil); err != nil {
+			assert.Error(t, err)
+		}*/
 }
